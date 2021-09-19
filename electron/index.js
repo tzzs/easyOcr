99d675a23357f59f763f415e6bff630bec75e21e
TZZ <1789803837@qq.com>
2021-09-20 01:17:14 +0800

    update preload

--- electron/index.js
+++ electron/index.js
@@ -1,9 +1,13 @@
 const {app, BrowserWindow} = require('electron')
+const path = require("path");
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
@@ -11,4 +15,8 @@ function createWindow() {
 
 app.whenReady().then(() => {
     createWindow()
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
 })
